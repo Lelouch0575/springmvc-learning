32770f874c83b458c593a76437ad2a6484a79dc6
MakiseKu <1554148583@qq.com>
2020-05-18 18:00:10 +0800

    add 04

---
+++ springmvc-04-controller/src/main/java/com/kuang/controller/ControllerTest1.java
@@ -0,0 +1,19 @@
+package com.kuang.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//实现了controller接口的类就是一个控制器
+public class ControllerTest1 implements Controller {
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        ModelAndView mv = new ModelAndView();
+
+        mv.addObject("msg","ControllerTest1");
+        mv.setViewName("test");
+
+        return mv;
+    }
+}
