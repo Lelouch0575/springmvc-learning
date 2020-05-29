e1cad238c51dea2d630b00916a04256254619852
MakiseKu <1554148583@qq.com>
2020-05-29 16:35:08 +0800

    complete ssmbuild

--- ssmbuild/src/main/java/com/kuang/controller/BookController.java
+++ ssmbuild/src/main/java/com/kuang/controller/BookController.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.xml.bind.SchemaOutputResolver;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -39,5 +42,43 @@ public class BookController {
         System.out.println("addBook=>" + books);
         bookService.addBook(books);
         return "redirect:/book/allBook";
+    }
+
+    //跳转到修改页面
+    @RequestMapping("/toUpdate")
+    public String toUpdatePaper(int id,Model model) {
+        Books books = bookService.queryBookById(id);
+        model.addAttribute("QBooks",books);
+        return "updateBook";
+    }
+
+    //修改书籍
+    @RequestMapping("/updateBook")
+    public String updateBook(Books books) {
+        System.out.println("update=>" + books);
+        bookService.updateBook(books);
+        return "redirect:/book/allBook";
+    }
+
+    //删除书籍
+    @RequestMapping("/deleteBook/{bookID}")
+    public String deleteBook(@PathVariable("bookID") int id) {
+        bookService.deleteBookById(id);
+        return "redirect:/book/allBook";
+    }
+
+    //查询书籍
+    @RequestMapping("queryBook")
+    public String queryBook(String queryBookName,Model model) {
+        Books books = bookService.queryBookByName(queryBookName);
+
+        List<Books> list = new ArrayList<>();
+        list.add(books);
+        if(books == null) {
+            list = bookService.queryAllBook();
+            model.addAttribute("error","未查到");
+        }
+        model.addAttribute("list",list);
+        return "allBook";
     }
 }
