46f29c3bbe285da879f960145ee1c405db913087
MakiseKu <1554148583@qq.com>
2020-05-31 17:08:19 +0800

    ajax2

--- springmvc-06-ajax/src/main/java/com/kuang/controller/AjaxController.java
+++ springmvc-06-ajax/src/main/java/com/kuang/controller/AjaxController.java
@@ -1,12 +1,37 @@
 package com.kuang.controller;
 
+import com.kuang.pojo.User;
+import com.sun.deploy.nativesandbox.comm.Response;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class AjaxController {
     @RequestMapping("/t1")
     public String test() {
         return "hello";
+    }
+
+    @RequestMapping("/a1")
+    public void a1(String name, HttpServletResponse response) throws IOException {
+        System.out.println("a1=>param " + name);
+        if("kuangshen".equals(name)) {
+            response.getWriter().print("true");
+        }else {
+            response.getWriter().print("false");
+        }
+    }
+
+    @RequestMapping("/a2")
+    public List<User> a2() {
+        List<User> userList = new ArrayList<>();
+        userList.add(new User("alex",1,"male"));
+        userList.add(new User("bob",2,"female"));
+        return userList;
     }
 }
--- springmvc-06-ajax/web/index.jsp
+++ springmvc-06-ajax/web/index.jsp
@@ -1,16 +1,24 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: kirito
-  Date: 2020/5/29
-  Time: 16:47
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head>
     <title>$Title$</title>
+    <script src="${pageContext.request.contextPath}/static/js/jquery-3.5.1.js"></script>
+    <script>
+      function a() {
+        $.post({
+          url: "${pageContext.request.contextPath}/a1",
+          data: {"name":$("#username").val()},
+          success: function (data,status) {
+            alert(data);
+            console.log("data=" + data);
+            console.log("status=" + status);
+          }
+        })
+      }
+    </script>
   </head>
   <body>
-  $END$
+<%--  失去焦点的时候发起一个请求(携带信息)到后台--%>
+  username:<input type="text" id="username" onblur="a()">
   </body>
 </html>
